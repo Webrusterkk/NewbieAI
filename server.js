256935ec3139e3f7433342a246910dd77ebc5aa5
Krishna Kishore Tummala <tummalk@altria.com>
2018-09-25 13:38:49 +0530

    Added Server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const express = require('express');
+const app = express();
 app.get('/*all', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
